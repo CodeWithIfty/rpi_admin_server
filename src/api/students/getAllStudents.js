27e7729674eed41130c65648311be609fb8c8140
CodeWithIftekhar <codewithiftekhar@gmail.com>
2024-06-07 01:57:24 +0600

    git students route

--- src/api/students/getAllStudents.js
+++ src/api/students/getAllStudents.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const { student_info } = require("../../models");
 
 const getAllStudents = async (req, res) => {
@@ -5,12 +6,50 @@ const getAllStudents = async (req, res) => {
     // Get page and limit from query parameters, default to page 1 and limit 10
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
+    const searchText = req.query.searchText
+      ? req.query.searchText.toString()
+      : null;
+    const departments = req.query.department
+      ? req.query.department.split(",")
+      : [];
+    const shift_group = req.query.shift_group
+      ? req.query.shift_group.split(",")
+      : [];
+    const semester = req.query.semester ? req.query.semester.split(",") : [];
+    const gender = req.query.gender || "";
 
-    // Calculate the offset
+    // Calculate offset for pagination
     const offset = (page - 1) * limit;
 
-    // Fetch students with pagination
+    let whereClause = {};
+
+    // Add searchText conditions
+    if (searchText) {
+      whereClause[Op.or] = [
+        { present_education_roll: { [Op.like]: `%${searchText}%` } },
+        { student_mobile_number: { [Op.like]: `%${searchText}%` } },
+        { student_name_english: { [Op.like]: `%${searchText}%` } },
+        { account_number: { [Op.like]: `%${searchText}%` } },
+      ];
+    }
+
+    // Add department and shift_group conditions
+    if (departments.length > 0) {
+      whereClause.present_education_department = departments;
+    }
+    if (shift_group.length > 0) {
+      whereClause.present_education_shift = shift_group;
+    }
+    if (semester.length > 0) {
+      whereClause.present_education_semester = semester;
+    }
+    if (gender) {
+      whereClause.gender = gender;
+    }
+
+    // Fetch students with pagination and filtering
     const { count, rows: students } = await student_info.findAndCountAll({
+      where: whereClause,
       limit,
       offset,
     });
@@ -18,11 +57,12 @@ const getAllStudents = async (req, res) => {
     // Calculate total pages
     const totalPages = Math.ceil(count / limit);
 
-    res.status(200).json({
-      students,
-      totalPages,
-      currentPage: page,
-    });
+    if (students.length > 0) {
+      return res.status(200).json({ students, totalPages, currentPage: page });
+    } else {
+      // No students found with the given searchText
+      return res.status(404).json({ message: "Students not found" });
+    }
   } catch (error) {
     console.error("Error fetching students:", error);
     res.status(500).json({ message: "Error fetching students" });
